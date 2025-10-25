e94a808a78e06a4ee7b1eafcb07f0c3090a4aec9
ANAM <khoirul130203@gmail.com>
2025-10-25 23:31:27 +0700

    update index

--- index.js
+++ index.js
@@ -1,15 +1,17 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
+app.get("/", (req, res) => {
+  res.send("Hello World!");
 });
 
-app.get('/about', (req, res) => {
-    res.send('About Page Test');
+app.get("/about", (req, res) => {
+  res.send("About Page Test");
 });
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
 });
+
+// for note
